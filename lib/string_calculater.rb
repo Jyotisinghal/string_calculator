8eb45b269fa84986da9d87726ec07d04fdf273bb
Jyotisinghal <jsinghal694@gmail.com>
2024-04-29 15:55:06 +0530

    condition satisfied for negative and string input

--- lib/string_calculater.rb
+++ lib/string_calculater.rb
@@ -1,6 +1,9 @@
 class StringCalculator
   def add(input)
+    return 'only accepts a string' unless input.is_a?(String)
     numbers = input.split(",").map(&:to_i)
+    negatives = numbers.select{ |i| i < 0 }
+    return 'negative value are not accepted' if negatives.length > 0
     return numbers.sum
   end
 end
--- spec/string_calculater_spec.rb
+++ spec/string_calculater_spec.rb
@@ -11,5 +11,13 @@ describe "StringCalculator" do
     it "returns the sum of two numbers" do
       expect(calculator.add("2,5")).to eql(7)
     end
+
+    it "returns 'only accepts a string' when not string" do
+      expect(calculator.add(2)).to eql('only accepts a string')
+    end
+
+    it "returns the sum of two numbers" do
+      expect(calculator.add("-2,5")).to eql('negative value are not accepted')
+    end
   end
 end
